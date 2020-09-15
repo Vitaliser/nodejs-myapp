d16c392157d8c884d4cd1630bb74c903a7f9c6d1
Vitili <nazanin_russel@yahoo.com>
2020-09-15 15:31:42 +0430

    Add configuration file for sequelize, and the init script

---
+++ config/config.js
@@ -0,0 +1,24 @@
+modules.export={
+  "development": {
+    "username": "express-mvp-dbuser",
+    "password": 123456,
+    "database": "express-mvp-db",
+    "host": "192.168.5.12",
+    "dialect": "postgres",
+    "port" :5432
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
