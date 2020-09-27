a65fe3504c488d86c772ff996f05cf929b48cb85
Vitili <nazanin_russel@yahoo.com>
2020-09-27 15:55:42 +0330

    Implemented signup, login and logout routes
    Using passportJS
    fixing some issues from before

--- controllers/user.js
+++ controllers/user.js
@@ -10,6 +10,16 @@
  *    (__ __)//
  */
 
+let models = require("../models");
+let bcrypt = require("bcrypt");
+let flash = require("connect-flash");
+const passport = require("passport");
+const myPassport = require("../passport_setup")(passport);
+
+const generateHash = function (password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
 exports.show_login = function (req, res, next) {
 	res.render('user/login', {formData: {}, errors: {}});
 }
@@ -19,9 +29,29 @@ exports.show_signup = function (req, res, next) {
 }
 
 exports.signup = function (req, res, next) {
-
+	const newUser = models.User.build(
+			{
+				email: req.body.email,
+				password: generateHash(req.body.password)
+			});
+	return newUser.save().then(result => {
+		passport.authenticate('local', {
+			successRedirect: "/",
+			failureRedirect: "/signup",
+			failureFlash: true
+		})(req, res, next)
+	})
 }
 
 exports.login = function (req, res, next) {
-
+	passport.authenticate('local', {
+		successRedirect: "/",
+		failureRedirect: "/login",
+		failureFlash: true
+	})(req, res, next);
+}
+exports.logout = function (req, res, next) {
+	req.logout();
+	req.session.destroy();
+	req.redirect('/');
 }
--- passport_setup.js
+++ passport_setup.js
@@ -34,29 +34,29 @@ module.exports = function (passport) {
 		})
 	});
 	passport.use(new LocalStrategy({
-			usernameField: 'email',
-			passwordField: 'password',
-			passReqToCallback: true
-		},
-		function (email, password, done) {
-			return models.User.findOne({
-				where: {
-					'email': email
-				},
-			}).then(user => {
-				if (user == null) {
-					req.flash('message', 'Incorrect credentials.')
-					return done(null, false);
-				} else if (user.password == null || user.password == undefined) {
-					req.flash('message', 'You must reset your password')
-					return done(null, false);
-				} else if (validPassword(user, password)) {
-					req.flash('message', 'Incorrect credentials')
-					return done(null, false);
-				}
-				return done(null, false)
-			}).catch(err => {
-				done(err, false)
-			})
+									   usernameField: 'email',
+									   passwordField: 'password',
+									   passReqToCallback: true
+								   },
+								   function (req, email, password, done) { //   Authenticate users
+									   return models.User.findOne({
+																	  where: {
+																		  'email': email
+																	  },
+																  }).then(user => {
+										   if (user == null) {
+											   req.flash('message', 'Incorrect credentials.')
+											   return done(null, false)
+										   } else if (user.password === null || user.password === undefined) {
+											   req.flash('message', 'You must reset your password')
+											   return done(null, false)
+										   } else if (!validPassword(user, password)) {
+											   req.flash('message', 'Incorrect credentials')
+											   return done(null, false)
+										   }
+										   return done(null, user);
+									   }).catch(err => {
+										   done(err, false);
+									   })
 		}))
 }
